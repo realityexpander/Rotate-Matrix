78173eb1a1460d738fa575f3795ac97cc0d53016
Chris Athanas <realityexpanderdev@gmail.com>
2018-11-27 15:43:39 -0600

    Update RotateMatrix.js

--- RotateMatrix.js
+++ RotateMatrix.js
@@ -1,4 +1,17 @@
 function rotateMatrix (matrix) {
-  // Write your code here, and
-  // return your final answer.
+  
+  let res = [];  
+  let size = matrix.length;
+
+  for(let i=size-1; i>=0; i--) {
+    res[i] = [];
+    for(let j=0; j<size; j++) {
+      res[i][j] = matrix[size-1-j][i];
+    }
+  }
+
+  return res;
+
 }
+
+console.log(rotateMatrix([ [ 1, 2, 3, 4 ], [ 5, 6, 7, 8 ], [ 9, "A", "B", "C" ], [ "D", "E", "F", "G" ] ]));
